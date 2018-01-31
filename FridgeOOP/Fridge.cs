55fb33b769b78059c9edb1bd4a8f120912e71122
JuliePretzlaff <juliapretzlaff@gmail.com>
2018-01-31 13:41:50 -0500

    Initial Commit

---
+++ FridgeOOP/Fridge.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FridgeOOP
+{
+    public class Fridge
+    {
+        //fields
+        private bool isClean;
+        private string dispenserType;
+        private int foodAmount;
+        private bool bulbsWork;
+
+
+    //Properties
+
+    
+    //Constructors
+
+
+    //Methods()
+
+
+
+
+    }
+}
