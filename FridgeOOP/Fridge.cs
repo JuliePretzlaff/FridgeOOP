3b2c65f4a510ed468c23faae81312ab632c1ebb2
JuliePretzlaff <juliapretzlaff@gmail.com>
2018-01-31 14:43:46 -0500

    created one method

--- FridgeOOP/Fridge.cs
+++ FridgeOOP/Fridge.cs
@@ -15,36 +15,70 @@ namespace FridgeOOP
         private bool bulbsWork;
 
 
-    //Properties
-    public bool IsClean
-        {
-            get { return this.isClean; }
-            set { this.isClean = value; }
-        }
+        //Properties
+        public bool IsClean
+            {
+                get { return this.isClean; }
+                set { this.isClean = value; }
+            }
 
-    public string DispenserType
-        {
-            get { return this.dispenserType; }
-        }
+        public string DispenserType
+            {
+                get { return this.dispenserType; }
+            }
     
-    public int FoodAmount
-        {
-            get { return this.foodAmount; }
-            set { this.foodAmount = value; }
-        }
+        public int FoodAmount
+            {
+                get { return this.foodAmount; }
+                set { this.foodAmount = value; }
+            }
 
         public bool BulbsWork
+            {
+                get { return this.bulbsWork; }
+                set { this.bulbsWork = value; }
+            }
+        
+        //Constructors
+        public Fridge()
         {
-            get { return this.bulbsWork; }
-            set { this.bulbsWork = value; }
+            this.dispenserType = "basic water dispenser";//this ensures every fridge instantiated will have this value for dispenserType
+            //this is the default constructor
         }
-    //Constructors
 
 
-        //Methods()
+        public Fridge(string dispenserType)
+        {
+            this.dispenserType = dispenserType; //first one is field, second one paramenter(in paranthese above);
+        }//the parameter is bypassing the property (which doesn't have a set) and going directly to the field, even though it's private (b/c the constructor is public)
+        
+   
+        public Fridge(bool isClean, string dispenserType, int foodAmount, bool bulbsWork)
+        {
+            this.isClean = isClean;
+            this.dispenserType = dispenserType;
+            this.foodAmount = foodAmount;
+            this.bulbsWork = bulbsWork;
+        }
 
+        //Methods() 
+        //a way to dispense water
+        public void DispenseWater()
+        {
+            if (dispenserType == "basic water dispenser")
+            {
+                Console.WriteLine("Here is some lukewarm water!");
+            }
+            else if(dispenserType == "deluxe dispenser")
+            {
+                Console.WriteLine("Enjoy your perfect ice water!");
+            }
+            else
+            {
+                Console.WriteLine("What's gonna come out?  It's a surprise!");
+            }
 
-
-
+            //
+        }
     }
 }
